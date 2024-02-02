b485228d714f690d7a2628e1819c86717a46d471
Juri Turiano <51355688+tJuri-sub@users.noreply.github.com>
2024-02-02 21:27:46 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+    },
+    fontSize: {
+      sm: '0.8rem',
+      base: '25px',
+      xl: '1.25rem',
+      '2xl': '64px',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
