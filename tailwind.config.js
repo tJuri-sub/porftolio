31e309e8593ae45070b21e9cb0c8d094f2b580b4
Juri Turiano <juricuber@gmail.com>
2024-02-12 01:47:56 +0800

    fresh start

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,29 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    container: {
-      center: true,
+    extend: {
+      container:{
+        center: true,
+      },
+      screens: {
+        xs: "425px",
+      },
+       colors: {
+        'primary': '#141414',
+        'secondary': '#ffffff',
+        'accent': '#9DA1F7',
+        'accent-btn': '#D98787',
+        'card': '#272727',
+        'buttons': '#3E3E3E',
+        'glass': '#2D2525',
+      },
+      fontFamily: {
+        'primary': 'Outfit',
+      },
+      boxShadow: {
+        'accent-shadow': '0px 4px 0px 0px #4B2C2C', 
+      }
     },
-    fontSize: {
-      sm: '0.8rem',
-      base: '25px',
-      xl: '1.25rem',
-      '2xl': '64px',
-      '3xl': '1.953rem',
-      '4xl': '2.441rem',
-      '5xl': '3.052rem',
-    },
-    boxShadow: {
-      'shadow': '0 3px 10px 0px #4B2C2C',
-    },
-    extend: {},
   },
   plugins: [],
 }
